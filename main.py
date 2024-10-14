d079c3235bec9b8d71e33aa26de8e67db4647c1c
toilasam <163370211+toilasam@users.noreply.github.com>
2024-10-14 20:59:20 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,47 @@
-from book_loan_requests import Request_manager
-from publishers import *
-from confirm import confirm_exit
 
-from Doc_gia import *
-from son import *
-from thanh import *
+from books import Books
+from book_loan_requests import Book_loan_requests, Request_manager
+from categories import Categories
+from customers import Customers
+from publishers import Publishers
+
 
 class_library = {
-    "1": 'books',
-    "2": 'requests',
-    "3": 'categories',
-    "4": 'customers',
-    "5": 'publishers'
+    "1": Books,
+    "2": Book_loan_requests,
+    "3": Categories,
+    "4": Customers,
+    "5": Publishers
 }
 
 if __name__ == "__main__":
-    ds_doc_gia = []  # Khởi tạo danh sách độc giả
-    # Bạn có thể khởi tạo một số độc giả mẫu nếu cần
-    doc_gia1 = DocGia(ma='001', ten='John Doe', email='john@example.com', sdt='123456789')
-    ds_doc_gia.append(doc_gia1)
-
-
     while True:
-        print('-------------Chương Trình Quản Lý Thư Viện------------')
-        Request_manager.notifications_late_loans(Request_manager.check_loans())
+        print('-----------Chương Trình Quản Lý Thư Viện------------')
         print(''' 
-            1: Sách             4: Độc giả
-            2: Phiếu mượn       5: Nhà xuất bản
-            3: Thể loại  ''')
-        user_input = input("Nhập mã đối tượng muốn tương tác ('q' để thoát): ").strip().lower()
-        if user_input == 'q':
-            confirm_exit()
-            
-        elif user_input in class_library: # or user_input in list(class_library.values()): 
-            if user_input == '1' or user_input == 'books': 
-                print('Sách')
-                menu()
-            elif user_input == '2' or user_input == 'requests':
-                print('Phiếu mượn')
+            1: Books          4: Customers
+            2: Requests       5: Publishers
+            3: Categories  ''')
+        user_input = input("Nhập mã đối tượng muốn tương tác ('exit' để thoát): ")
+        if user_input == 'exit':
+            print("Bye")
+            break
+
+        if user_input in class_library:
+            if user_input == '1': #### ví dụ nhập 1 để tương tác với đối tượng sách:
+                print('Books')
+                pass
+            if user_input == '2':#### ví dụ nhập 2 để tương tác với đối tượng phiếu mượn sách:
+                print('Requests')
                 Request_manager.choose_action()
-            elif user_input == '3': 
-                menu_quan_ly()
-                # pass
-            elif user_input == '4':
-                doc_gia = DocGia(ma='001', ten='John Doe', email='john@example.com', sdt='123456789')
-                doc_gia.chon_chuc_nang(ds_doc_gia)  # Gọi phương thức từ đối tượng và truyền ds_doc_gia
-            elif user_input == '5':
+            ##### mọi người điền tiếp ÌF để tương tác với phần bài của mọi người dưới đây ####
+            if user_input == '5':
+                print('nha_XB')
                 Publishers.menu()
+                Publishers.doc_file()
+                
         else:
-            print("Đối tượng không tồn tại. Vui lòng chọn lại.")
+            print("Đối tượng không tồn tại. Vui lòng thử lại.")
+
+    print('Thoát chương trình.')
+
 
